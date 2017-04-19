5a7a4ba4a48a212c7627a2569e834c0969a82901
alperozisik <smartface9@users.smarfacecloud.io>
2017-04-19 08:22:28 +0000

    fix version number

--- scripts/pages/page1.js
+++ scripts/pages/page1.js
@@ -1,5 +1,15 @@
 const extend = require("js-base/core/extend");
 const Router = require("nf-core/ui/router");
+const nw = require("nw-smf");
+nw.registerService(require("../definitions/sample"));
+nw.baseURL = "https://jslibs.azurewebsites.net/examples/nw/";
+nw.commonHeaders["Accept"] = nw.commonHeaders["Content-Type"] = "application/json";
+nw.onActivityStart = function() {
+    console.log("starting activity");
+};
+nw.onActivityEnd = function() {
+    console.log("activity ends");
+};
 
 // Get generetad UI code
 var Page1Design = require("../ui/ui_page1");
@@ -12,7 +22,7 @@ const Page1 = extend(Page1Design)(
         this.mapChildren(function(component, componentName) {
             self[componentName] = component;
         });
-        
+
         this.flexlayout.mapChildren(function(component, componentName) {
             self.flexlayout[componentName] = component;
         });
@@ -32,6 +42,65 @@ var btnClickCount = 0;
 
 // Gets/sets press event callback for btn
 function btn_onPress() {
+    var Redux = require("redux/dist/redux.min");
+
+    function counter(state = 0, action) {
+        switch (action.type) {
+            case 'INCREMENT':
+                return state + 1;
+            case 'DECREMENT':
+                return state - 1;
+            default:
+                return state;
+        }
+    }
+
+    // Create a Redux store holding the state of your app.
+    // Its API is { subscribe, dispatch, getState }.
+    let store = Redux.createStore(counter);
+
+    // You can use subscribe() to update the UI in response to state changes.
+    // Normally you'd use a view binding library (e.g. React Redux) rather than subscribe() directly.
+    // However it can also be handy to persist the current state in the localStorage.
+
+    store.subscribe(function() {
+        console.log(store.getState());
+    });
+
+    // The only way to mutate the internal state is to dispatch an action.
+    // The actions can be serialized, logged or stored and later replayed.
+    store.dispatch({
+        type: 'INCREMENT'
+    });
+    // 1
+    store.dispatch({
+        type: 'INCREMENT'
+    });
+    // 2
+    store.dispatch({
+        type: 'DECREMENT'
+    });
+    // 1
+
+    var loginInfo = {
+        user: "smartface",
+        password: "Cloud1Code"
+    };
+    nw.factory("login").body(loginInfo).result(function(err, data) {
+        if (err) {
+            console.log("error");
+            if (typeof err === "object")
+                err = JSON.stringify(err, null, "\t");
+            console.log(err);
+        }
+        else {
+            console.log("success");
+            if (typeof data === "object")
+                data = JSON.stringify(data, null, "\t");
+            console.log(data);
+        }
+    }).run();
+
     var myLabelText = "";
     var myButtonText = "";
 
